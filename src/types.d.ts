3b0f3f72e778cb6855b471acdfde5a61d8d4cd96
Bojan Stankovic <dev.bojan.stankovic@gmail.com>
2025-06-26 23:07:04 +0200

    #35 Updated package dependencies and added package module and class declarations/exports.

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,24 @@ type LocalStorageItem = {
     data: any,
     expiry: number | null
 };
+
+declare module '@bjnstnkvc/local-storage' {
+    export class LocalStorage {
+        static ttl(value: number | null): void;
+        static set(key: string, value: any, ttl?: number | null): void;
+        static get(key: string, fallback?: string | Function | null): any;
+        static remember(key: string, callback: Function, ttl?: number | null): any;
+        static all(): object;
+        static remove(key: string): void;
+        static clear(): void;
+        static has(key: string): boolean;
+        static hasAny(keys: string | string[]): boolean;
+        static isEmpty(): boolean;
+        static isNotEmpty(): boolean;
+        static keys(): string[];
+        static count(): number;
+        static touch(key: string, ttl?: number | null): void;
+        static expiry(key: string, asDate?: boolean): number | Date | null;
+        static dump(key: string): void;
+    }
+}
